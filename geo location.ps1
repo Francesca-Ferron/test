2b35c6f698544b439fe576acb313a04713a60084
Francesca-Ferron <156924007+Francesca-Ferron@users.noreply.github.com>
2024-09-18 15:39:10 +0200

    Add files via upload

---
+++ geo location.ps1
@@ -0,0 +1,13 @@
+Add-Type -AssemblyName System.Device
+$GeoWatcher = New-Object System.Device.Location.GeoCoordinateWatcher(1)
+$GeoWatcher.Start()
+
+while (($GeoWatcher.Status -ne 'Ready') -and ($GeoWatcher.Permission -ne 'Denied')) {
+    Start-Sleep -Milliseconds 100
+}
+$location = $GeoWatcher.Position.Location
+$latitude = $location.Latitude
+$longitude = $location.Longitude
+$googleMapsUrl = "https://www.google.com/maps?q=$latitude,$longitude"
+
+$googleMapsUrl | Out-File -FilePath "C:\! Cynet Ransom Protection(DON'T DELETE)\google.txt" -Encoding UTF8
